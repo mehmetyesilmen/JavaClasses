d3c6d0eaed31aabd7898b3bb8e8544821dc9de04
mehmetyesilmen <yesilmenmehmet47@gmail.com>
2019-11-14 14:58:07 -0600

    last push to github

---
+++ src/reviwePackege2/whileLoop.java
@@ -0,0 +1,126 @@
+package reviwePackege2;
+
+import java.util.Scanner;
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		
+//		int a=50;
+//		
+//		while(a<=100) {
+//			System.out.print(a+" ");
+//			
+//			a+=2;
+//			
+//		}
+//		
+//		
+//		int b=100;
+//		
+//		while(b>=1) {
+//			if(b%2==0) {
+//				System.out.println(b);
+//				
+//			}
+//			b--;
+//		}
+		
+//		
+//		boolean workday=true;
+//		int day=1;
+//        
+//		
+//		while(workday) {
+//				System.out.println("I need a day off");
+//				
+//				if (day<6 ) {
+//					System.out.println("I do not need a day off anymore");
+//					workday=false;
+//				}
+//				
+//			day++;
+//				
+//			}
+//			
+//		int i=20;
+//		
+//		do {
+//			
+//			if(i%2==0);{
+//			System.out.print(i+" ");
+//			i++;
+//			}
+//			
+//		}while(i<=50);
+		
+		
+		
+		
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("pleace enter thh price");
+//		
+//		int price;
+//		price=scan.nextInt();
+//		
+//		while(price!=5){
+//			System.out.println("pleace enter the price");
+//			
+//			price=scan.nextInt();
+//			
+//		}
+//		
+//		System.out.println("enjoy");
+//		
+		
+		
+		
+//		System.out.println("please enter your tea price");
+//		
+//		int coast;
+//		coast=scan.nextInt();
+//		
+//		while(coast!=10) {
+//			System.out.println("please pay your money");
+//			
+//			coast=scan.nextInt();
+//		}
+//		
+//		System.out.println("enjoy your tea");
+				
+//		System.out.println("please enter a number");
+		
+//		int fac=scan.nextInt();
+//		
+//		int number=5;
+//		int fac=1;
+//	
+//		for(int i=1; i<=number; i++) {
+//			
+//			fac=fac*i;
+//			
+//			
+//		}
+//		System.out.println(fac);
+//    }
+	
+	String str="Babam bana bisiklet aldi";
+	
+	String reverse="";
+	
+	int len=str.length();{
+	
+	for(int i=str.length()-1; i>=0; i--){
+		
+		
+		reverse=reverse+str.charAt(i);
+		
+	}
+	System.out.println(reverse);
+	
+	}		
+	
+	}
+	}
+
+
