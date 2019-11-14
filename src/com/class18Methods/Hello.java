49f515736a3c610389b1876a31671e432c7e826e
mehmetyesilmen <yesilmenmehmet47@gmail.com>
2019-11-14 15:00:09 -0600

    last push to github

--- src/com/class18Methods/Hello.java
+++ src/com/class18Methods/Hello.java
@@ -13,9 +13,67 @@ public class Hello {
 	public static void main(String [] args) {
 		
 		Hello obj=new Hello();
+		
 		obj.sayHello("Asel");
 		obj.sayHello("Waqas");
 		obj.sayHello("Diego");
+		obj.sayNameAndAge("John", 25);
+		obj.isSnowing(false);
+		obj.isSnowing(true);
+		obj.largerNumber(40,40);
+		obj.oddOrEven(20);
+		obj.oddOrEven(25);
+		
+		
+	    }
+	
+	//method to say age and name
+	
+	void sayNameAndAge(String name, int age) {
+		
+		System.out.println("My name is"+ name+ " and I am" +age +"years old");
+		
 	}
+	
+	// create a method that will check if it snowing
+	// if snow --> stay at home, otherwise go for a walk
+	
+	void isSnowing(boolean isSnowing) {
+		
+		if(isSnowing) {
+			System.out.println("Stay at home");
+			
+		}else {
+			System.out.println("Go for a walk");
+		}
+	  }
+	void largerNumber(int first, int second){
+		
+		if(first>second) {
+		   System.out.println("The larger number is "+ " "+first);
+			
+		}else if(first<second) {
+			System.out.println("The larger number is "+ " "+second);
+			
+		}else {
+			System.out.println("The numbers are equals");
+		}
+		
+	}
+	
+	void oddOrEven(int numbers){
+		
+		
+		if(numbers%2==0) {
+			System.out.println("The number is  even and it is "+numbers);
+		
+		}else {
+			System.out.println("The number is odd and it is "+numbers);
+		}
 
+	}
+	
+	
 }
+
+
